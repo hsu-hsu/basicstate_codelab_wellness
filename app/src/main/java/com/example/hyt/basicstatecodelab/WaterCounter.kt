64f7465c80c78e5d455ffa57b9e8e0837c407979
Hsu Yee Thin <hsuyeethin@rebbiz.com>
2025-01-18 20:57:40 +0630

    final code

--- app/src/main/java/com/example/hyt/basicstatecodelab/WaterCounter.kt
+++ app/src/main/java/com/example/hyt/basicstatecodelab/WaterCounter.kt
@@ -1,22 +1,38 @@
 package com.example.hyt.basicstatecodelab
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun WaterCounter(modifier: Modifier = Modifier) {
-    var count = 0;
-    Column(modifier = Modifier.padding(16.dp)) {
-        Text(
-            text = "You have had $count glasses."
-        )
-        Button(onClick = {count++}, Modifier.padding(top = 8.dp)) {
-            Text("Add One")
+fun StatelessCounter(count: Int, onIncrement: () -> Unit, modifier: Modifier = Modifier) {
+    Column(modifier = modifier.padding(16.dp)) {
+        if (count > 0) {
+            Text("You've had $count glasses.")
+        }
+        Button(onClick = onIncrement, Modifier.padding(top = 8.dp), enabled = count < 10) {
+            Text("Add one")
         }
     }
+}
+
+@Composable
+fun StatefulCounter(modifier: Modifier = Modifier) {
+    var waterCount by remember { mutableStateOf(0) }
+
+    var juiceCount by remember { mutableStateOf(0) }
+
+    StatelessCounter(waterCount, { waterCount++ })
+    StatelessCounter(juiceCount, { juiceCount++ })
 }
